914e664bea453f65af72db1614092d906bed2b37
Karl Eklund <localpart@gmail.com>
2021-12-14 11:24:48 +0100

    encoding tweak

--- Org Capture/Program.cs
+++ Org Capture/Program.cs
@@ -1,28 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
+using System.Text;
 
-Task logTask = File.WriteAllLinesAsync("C:\\Users\\kalle\\Org Capture.log", args);
 string orgLink = args[0];
-
-orgLink = orgLink.Replace("org-protocol://capture/", "org-protocol://capture");
-
-string emacsClient = @"C:\Users\kalle\scoop\shims\emacsclientw.exe";
-
-// Process.Start(emacsClient, orgLink);
-
-ProcessStartInfo processStartInfo = new ProcessStartInfo();
-
-processStartInfo.CreateNoWindow = true;
-processStartInfo.UseShellExecute = false;
-processStartInfo.FileName = emacsClient;
-processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-processStartInfo.Arguments = "-n " + orgLink;
-
-Process? exeProcess = Process.Start(processStartInfo);
-if(exeProcess != null)
+if(orgLink != null)
 {
-    exeProcess.WaitForExit();
+    orgLink = orgLink.Replace("org-protocol://capture/", "org-protocol://capture");
+    byte[] bytes = Encoding.UTF8.GetBytes(orgLink);
+    //Task logTask = File.WriteAllBytesAsync("C:\\Users\\kalle\\Org Capture.log", bytes);
+    orgLink = Encoding.Latin1.GetString(bytes);
+    Task logTask = File.WriteAllTextAsync("C:\\Users\\kalle\\Org Capture.log", orgLink);
+
+    string emacsClient = @"C:\Users\kalle\scoop\shims\emacsclientw.exe";
+
+    // Process.Start(emacsClient, orgLink);
+
+    ProcessStartInfo processStartInfo = new ProcessStartInfo();
+
+    processStartInfo.CreateNoWindow = true;
+    processStartInfo.UseShellExecute = false;
+    processStartInfo.FileName = emacsClient;
+    processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+    string arguments = "-n " + orgLink;
+    processStartInfo.Arguments = arguments;
+        
+    Process? exeProcess = Process.Start(processStartInfo);
+    if (exeProcess != null)
+    {
+        exeProcess.WaitForExit();
+    }
+    logTask.Wait();
 }
 
-logTask.Wait();
